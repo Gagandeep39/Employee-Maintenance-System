c2e91d0f73ed1588b69109275a85489ed1cee4eb
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-02-13 21:13:59 +0530

    Added a LeaveHistory Map dataset DeaImply constructor now loads data in leaveMap

--- EmployeeMaintenance/src/main/java/com/cg/repository/DataRepository.java
+++ EmployeeMaintenance/src/main/java/com/cg/repository/DataRepository.java
@@ -8,12 +8,14 @@ import java.util.Date;
 import java.util.HashMap;
 
 import com.cg.beans.Employee;
+import com.cg.beans.LeaveHistory;
 import com.cg.beans.User;
 import com.cg.enums.Department;
 import com.cg.enums.Designation;
 import com.cg.enums.Gender;
 import com.cg.enums.Grade;
 import com.cg.enums.GradeType;
+import com.cg.enums.LeaveStatus;
 import com.cg.enums.MaritalStatus;
 import com.cg.enums.UserType;
 
@@ -28,6 +30,7 @@ public class DataRepository {
 	private static HashMap<Integer, Employee> employeeMap;
 	private static HashMap<Integer, Department> departmentMap;
 	private static HashMap<GradeType, Grade> gradeMap;
+	private static HashMap<Integer, LeaveHistory> leaveMap;
 	static {
 		prepareHashMap();
 	}
@@ -39,10 +42,11 @@ public class DataRepository {
 		employeeMap = new HashMap<Integer, Employee>();
 		departmentMap = new HashMap<Integer, Department>();
 		gradeMap = new HashMap<GradeType, Grade>();
+		leaveMap = new HashMap<Integer, LeaveHistory>();
 		
-		User u1 = new User("gagandeep", "123456", UserType.Admin);
+		User u1 = new User("admin", "123456", UserType.Admin);
 		User u2 = new User("pranav", "123456", UserType.Employee);
-		User u3 = new User("palak", "123456", UserType.Admin);
+		User u3 = new User("gagan", "123456", UserType.Employee);
 		User u4 = new User("Tanmay", "123456", UserType.Employee);
 		User u5 = new User("uma", "123456", UserType.Employee);
 		
@@ -50,10 +54,11 @@ public class DataRepository {
 		
 
 			date = LocalDate.parse("1999-05-14");
-		
+		System.out.println(u3.getUserId());
 //		Employee e1 = new Employee(u1.getUserId(), "Gagandeep", "Singh", date, 1002, GradeType.M7, Designation.Manager, 100000, Gender.M, MaritalStatus.Single, "Planet earth", "12345677", 0);
-		Employee e2 = new Employee(u2.getUserId(), "pranav", "Singh", date, 1002, GradeType.M7,Designation.Developer, 100000, Gender.M, MaritalStatus.Single, "Planet earth", "12345677", 1001);
-	
+		Employee e2 = new Employee(u2.getUserId(), "pranav", "Singh", date, 1002, GradeType.M7,Designation.Developer, 100000, Gender.M, MaritalStatus.Single, "Planet earth", "12345677", 1003);
+		Employee e3 = new Employee(u3.getUserId(), "Gagandeep", "Singh", date, 1002, GradeType.M7,Designation.Manager, 100000, Gender.M, MaritalStatus.Single, "Planet earth", "12345677", 0);
+		
 		
 		
 		userMap.put(u1.getUserId(), u1);
@@ -65,6 +70,7 @@ public class DataRepository {
 		
 //		employeeMap.put(e1.getEmpId(), e1);
 		employeeMap.put(e2.getEmpId(), e2);
+		employeeMap.put(e3.getEmpId(), e3);
 		
 		
 		departmentMap.put(1001, new Department(1001, "IT"));
@@ -81,6 +87,9 @@ public class DataRepository {
 		gradeMap.put(GradeType.M5, new Grade(GradeType.M5, "Intermediate Employee", 2000001, 2500000));
 		gradeMap.put(GradeType.M6, new Grade(GradeType.M6, "Professional Employee", 2500001, 3500000));
 		gradeMap.put(GradeType.M7, new Grade(GradeType.M7, "Highly Professional Employee", 3500001, 4000000));
+		
+		LeaveHistory l1 = new LeaveHistory(e2.getEmpId(), 12, 3, LocalDate.parse("2020-04-12"), LocalDate.parse("2020-04-15"), LeaveStatus.Applied);
+		leaveMap.put(l1.getLeaveId(), l1);
 		
 		
 	}
@@ -107,6 +116,11 @@ public class DataRepository {
 	
 	public static HashMap<Integer, Department> getDepartmentList() {
 		return departmentMap;
+	}
+	
+	public static HashMap<Integer, LeaveHistory> getLeaveMap() {
+		return leaveMap;
+		
 	}
 
 }
