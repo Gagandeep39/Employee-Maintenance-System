60346879b96a489806a31de8ac2041ace25179e4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-02-16 00:13:59 +0530

    Cleaned Up data in repository

--- EmployeeMaintenance/src/main/java/com/cg/repository/DataRepository.java
+++ EmployeeMaintenance/src/main/java/com/cg/repository/DataRepository.java
@@ -25,7 +25,7 @@ import com.cg.enums.UserType;
  * @date 10-Feb-2020
  */
 public class DataRepository {
-	
+
 	private static HashMap<Integer, User> userMap;
 	private static HashMap<Integer, Employee> employeeMap;
 	private static HashMap<Integer, Department> departmentMap;
@@ -34,6 +34,7 @@ public class DataRepository {
 	static {
 		prepareHashMap();
 	}
+
 	/**
 	 * 
 	 */
@@ -43,41 +44,35 @@ public class DataRepository {
 		departmentMap = new HashMap<Integer, Department>();
 		gradeMap = new HashMap<GradeType, Grade>();
 		leaveMap = new HashMap<Integer, LeaveHistory>();
-		
-		User u1 = new User("admin", "123456", UserType.Admin);
-		User u2 = new User("pranav", "123456", UserType.Employee);
-		User u3 = new User("gagan", "123456", UserType.Employee);
-		User u4 = new User("Tanmay", "123456", UserType.Employee);
-		User u5 = new User("uma", "123456", UserType.Employee);
-		
-		LocalDate date = null;
-		
 
-			date = LocalDate.parse("1999-05-14");
-			Employee e2 = new Employee(u2.getUserId(), "pranav", "Singh", date, 1002, GradeType.M7,Designation.Developer, 100000, Gender.M, MaritalStatus.Single, "Planet earth", "12345677", 1003);
-		Employee e3 = new Employee(u3.getUserId(), "Gagandeep", "Singh", date, 1002, GradeType.M7,Designation.Manager, 100000, Gender.M, MaritalStatus.Single, "Planet earth", "12345677", 0);
+		User u1 = new User("admin", "123456", UserType.Admin);
+		User u2 = new User("gagan", "123456", UserType.Employee);
+		User u3 = new User("pranav", "123456", UserType.Employee);
+		User u4 = new User("palak", "123456", UserType.Employee);
 		
-		
-		
+		Employee e2 = new Employee(u2.getUserId(), "Gagandeep", "Singh", LocalDate.parse("1999-05-14"), 1002, GradeType.M7, Designation.Manager,
+				100000, Gender.M, MaritalStatus.Single, "Planet earth", "12345677", 0);
+		Employee e3 = new Employee(u3.getUserId(), "Pranav", "Karmarkar", LocalDate.parse("1998-03-21"), 1002, GradeType.M7, Designation.Developer,
+				100000, Gender.M, MaritalStatus.Single, "Planet earth", "12345677", 100002);
+		Employee e4 = new Employee(u3.getUserId(), "Palak", "Surname", LocalDate.parse("1998-03-21"), 1002, GradeType.M7, Designation.Developer,
+				100000, Gender.F, MaritalStatus.Single, "Planet earth", "12345677", 100002);
+
 		userMap.put(u1.getUserId(), u1);
 		userMap.put(u2.getUserId(), u2);
 		userMap.put(u3.getUserId(), u3);
 		userMap.put(u4.getUserId(), u4);
-		userMap.put(u5.getUserId(), u5);
-		
-		
-//		employeeMap.put(e1.getEmpId(), e1);
+
 		employeeMap.put(e2.getEmpId(), e2);
 		employeeMap.put(e3.getEmpId(), e3);
-		
-		
+		employeeMap.put(e4.getEmpId(), e4);
+
 		departmentMap.put(1001, new Department(1001, "IT"));
 		departmentMap.put(1002, new Department(1002, "HR"));
 		departmentMap.put(1003, new Department(1003, "Accounts"));
 		departmentMap.put(1004, new Department(1004, "Marketing"));
 		departmentMap.put(1005, new Department(1005, "Training"));
 		departmentMap.put(1006, new Department(1006, "Troubleshooting"));
-		
+
 		gradeMap.put(GradeType.M1, new Grade(GradeType.M1, "Beginner", 250000, 400000));
 		gradeMap.put(GradeType.M2, new Grade(GradeType.M2, "Trainee", 400001, 1000000));
 		gradeMap.put(GradeType.M3, new Grade(GradeType.M3, "Employee", 1000001, 1500000));
@@ -85,17 +80,17 @@ public class DataRepository {
 		gradeMap.put(GradeType.M5, new Grade(GradeType.M5, "Intermediate Employee", 2000001, 2500000));
 		gradeMap.put(GradeType.M6, new Grade(GradeType.M6, "Professional Employee", 2500001, 3500000));
 		gradeMap.put(GradeType.M7, new Grade(GradeType.M7, "Highly Professional Employee", 3500001, 4000000));
-		
-		LeaveHistory l1 = new LeaveHistory(e2.getEmpId(), 12, 3, LocalDate.parse("2020-04-12"), LocalDate.parse("2020-04-15"), LeaveStatus.Applied);
+
+		LeaveHistory l1 = new LeaveHistory(e2.getEmpId(), 12, 3, LocalDate.parse("2020-04-12"),
+				LocalDate.parse("2020-04-15"), LeaveStatus.Applied);
 		leaveMap.put(l1.getLeaveId(), l1);
-		
-		
+
 	}
-	
+
 	public static HashMap<GradeType, Grade> getGradeList() {
 		return gradeMap;
 	}
-	
+
 	/**
 	 * 
 	 */
@@ -103,7 +98,7 @@ public class DataRepository {
 		return userMap;
 
 	}
-	
+
 	/**
 	 * 
 	 */
@@ -111,14 +106,14 @@ public class DataRepository {
 		return employeeMap;
 
 	}
-	
+
 	public static HashMap<Integer, Department> getDepartmentList() {
 		return departmentMap;
 	}
-	
+
 	public static HashMap<Integer, LeaveHistory> getLeaveMap() {
 		return leaveMap;
-		
+
 	}
 
 }
