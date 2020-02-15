ccab66d329bb3cefbd33068516d455051ca1898a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-02-15 22:09:07 +0530

    Removed Unecessary Import

--- EmployeeMaintenance/src/main/java/com/cg/service/AdminService.java
+++ EmployeeMaintenance/src/main/java/com/cg/service/AdminService.java
@@ -8,12 +8,10 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.cg.beans.Employee;
-import com.cg.beans.User;
 import com.cg.enums.Department;
 import com.cg.enums.Designation;
 import com.cg.enums.Grade;
 import com.cg.enums.GradeType;
-import com.cg.exception.UserNotFoundException;
 import com.cg.repository.DataRepository;
 
 /**
@@ -32,50 +30,54 @@ public interface AdminService {
 	boolean deleteEmployee(int empId);
 
 	Employee searchEmployee(int empId);
-	
+
 	int modifyManager(int empId, int managerId);
 
 	List<Employee> showAllEmployees();
 
-
-	//Validations
+	// Validations
 	String namePattern = "[A-Za-z]{4,}";
 	String mobilePattern = "[7-9][0-9]{9}";
 	String emailPattern = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$";
-	String datePattern = "^\\d{4}-\\d{1,2}-\\d{1,2}$";	// '\\d'-> represents digit
+	String datePattern = "^\\d{4}-\\d{1,2}-\\d{1,2}$"; // '\\d'-> represents digit
 	String departmentPattern = "[1]";
 
 	static boolean validateName(String name) {
 		return name.matches(namePattern);
 	}
+
 	static boolean validateMobile(String mobile) {
 		return mobile.matches(mobilePattern);
 	}
+
 	static boolean validateEmail(String email) {
 		return email.matches(emailPattern);
 	}
+
 	static boolean validateDate(String dob) {
-		if(dob.matches(datePattern)) {
+		if (dob.matches(datePattern)) {
 			String[] date = dob.split("-");
-			Integer[] dayInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-			if(Integer.parseInt(date[1])>12) {
+			Integer[] dayInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+			if (Integer.parseInt(date[1]) > 12) {
 				System.out.println("Invalid month");
 				return false;
-			}else if(!(Integer.parseInt(date[2])<dayInMonth[Integer.parseInt(date[1])-1])) {
-				System.out.println("Month "+ date[1] + " has only " + dayInMonth[Integer.parseInt(date[1])-1] + " days");
+			} else if (!(Integer.parseInt(date[2]) < dayInMonth[Integer.parseInt(date[1]) - 1])) {
+				System.out.println(
+						"Month " + date[1] + " has only " + dayInMonth[Integer.parseInt(date[1]) - 1] + " days");
 				return false;
 			}
 			return true;
-		}
-		else {
+		} else {
 			System.out.println("Pattern must be (yyyy-mm-dd)");
 			return false;
 		}
 	}
+
 	static boolean validateDepartment(int num) {
-		List<Department> departmentList = DataRepository.getDepartmentList().values().stream().collect(Collectors.toList());
+		List<Department> departmentList = DataRepository.getDepartmentList().values().stream()
+				.collect(Collectors.toList());
 		for (Department department : departmentList) {
-			if(department.getDepartmentId()==num)
+			if (department.getDepartmentId() == num)
 				return true;
 		}
 		System.out.println("Invalid Department Number!!");
@@ -86,16 +88,17 @@ public interface AdminService {
 
 	/**
 	 * @param sal
-	 * @param empGrade 
+	 * @param empGrade
 	 * @return
 	 */
 	static boolean validateBasic(double sal, GradeType empGrade) {
 		List<Grade> gradeList = DataRepository.getGradeList().values().stream().collect(Collectors.toList());
-		Optional<Grade> grade = gradeList.stream().filter(g->g.getGradeCode()==empGrade).findAny();
-		if(sal>=grade.get().getMinSalary() && sal<=grade.get().getMaxSalary())
+		Optional<Grade> grade = gradeList.stream().filter(g -> g.getGradeCode() == empGrade).findAny();
+		if (sal >= grade.get().getMinSalary() && sal <= grade.get().getMaxSalary())
 			return true;
-		else 
-			System.out.println( empGrade + " Employee's salary must be between " + grade.get().getMinSalary() + " and  " + grade.get().getMaxSalary());
+		else
+			System.out.println(empGrade + " Employee's salary must be between " + grade.get().getMinSalary() + " and  "
+					+ grade.get().getMaxSalary());
 		return false;
 	}
 
@@ -104,14 +107,15 @@ public interface AdminService {
 	 * @return
 	 */
 	static boolean validateManager(int managerId) {
-		List<Employee> managerList = DataRepository.getEmployeeList().values().stream().filter(e->e.getEmpDesignation()==Designation.Manager).collect(Collectors.toList());
+		List<Employee> managerList = DataRepository.getEmployeeList().values().stream()
+				.filter(e -> e.getEmpDesignation() == Designation.Manager).collect(Collectors.toList());
 		for (Employee employee : managerList) {
-			if(employee.getEmpId()==managerId)
+			if (employee.getEmpId() == managerId)
 				return true;
 		}
 		System.out.println("*********List of Manager Name and ID*********");
-		managerList.forEach(m->{
-			System.out.println(m.getEmpId() +  " - " + m.getEmpFirstName() + " " + m.getEmpLastName());
+		managerList.forEach(m -> {
+			System.out.println(m.getEmpId() + " - " + m.getEmpFirstName() + " " + m.getEmpLastName());
 		});
 		return false;
 	}
@@ -123,7 +127,7 @@ public interface AdminService {
 	static boolean validateAge(LocalDate d) {
 		LocalDate currentDate = LocalDate.now();
 		Period period = Period.between(d, currentDate);
-		if(period.getYears()>=18 && period.getYears()<=58)
+		if (period.getYears() >= 18 && period.getYears() <= 58)
 			return true;
 		else {
 			System.out.println("Employee must be between 18-58 years");
